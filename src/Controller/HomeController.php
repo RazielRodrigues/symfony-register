e13159eb40e6d3afba31ab00333b4071efb77ae5
razielrodrigues <raziel@upoutsourcing.com>
2022-04-09 20:04:37 -0300

    login and register system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/home", name="app_home")
      */
     public function index(): Response
     {
