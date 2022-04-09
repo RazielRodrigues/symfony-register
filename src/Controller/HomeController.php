ceb95a02d1dba73e4a467c72e26504b51473bd6c
razielrodrigues <raziel@upoutsourcing.com>
2022-04-09 20:04:50 -0300

    wip

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/", name="app_home")
      */
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
