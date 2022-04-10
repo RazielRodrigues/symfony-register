e4d2cecc48b3df22c5cf1180a0d2170b5de1e924
razielrodrigues <raziel@upoutsourcing.com>
2022-04-10 00:48:09 -0300

    final frontend adjusts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,11 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_index")
      */
     public function index(): Response
+    {
+        return $this->render('home/home.html.twig');
+    }
+
+    /**
+     * @Route("/home", name="app_home")
+     */
+    public function home(): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('home/index.html.twig');
     }
+
 }
