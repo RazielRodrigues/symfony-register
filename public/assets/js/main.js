2d81ec08184ab57723b55ae92c211c8c65790bba
razielrodrigues <raziel@upoutsourcing.com>
2022-04-10 01:22:59 -0300

    speech improvement

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,56 +1,54 @@
-// window.onload = function(){
+window.onload = function(){
 
-// 	var recognition = new webkitSpeechRecognition();
-// 	recognition.lang = "pt-BR";
-// 	recognition.continuous = true;
+	var recognition = new webkitSpeechRecognition();
+	recognition.lang = "pt-BR";
+	recognition.continuous = true;
 
+	recognition.onresult = function(event){
 
-// 	recognition.onresult = function(event){
+		var busca = "";
+		for (var i = event.resultIndex; i < event.results.length; i++) {
 
-// 		var busca = "";
-// 		for (var i = event.resultIndex; i < event.results.length; i++) {
+			if (event.results[i].isFinal){
+				busca += event.results[i][0].transcript.trim().toLowerCase();
+			}
+		}
 
-// 			if (event.results[i].isFinal){
-// 				busca += event.results[i][0].transcript.trim().toLowerCase();
-// 			}
-// 		}
+		// if (busca.indexOf("buscar por") > -1) {
 
-// 		if (busca.indexOf("buscar por") > -1) {
+		// 	busca = busca.substring(11,busca.length);
 
-// 			busca = busca.substring(11,busca.length);
+		// 	let url = "https://www.google.com/search?q=" + busca;
 
-// 			let url = "https://www.google.com/search?q=" + busca;
+		// 	window.open(url);
 
-// 			window.open(url);
+		// }else{
 
-// 		}else{
+			// busca = busca.split(" ").join("+")
+			// let api_key = "IuFzLL4qAtOzICPwH4CRpTIygyE8y4ms";
+			// let url = "https://api.giphy.com/v1/gifs/search?q="+busca+"&api_key="+api_key+"&limit=5";
+			// var xhr = $.get(url);
 
-// 			busca = busca.split(" ").join("+")
-// 			let api_key = "IuFzLL4qAtOzICPwH4CRpTIygyE8y4ms";
-// 			let url = "https://api.giphy.com/v1/gifs/search?q="+busca+"&api_key="+api_key+"&limit=5";
-// 			var xhr = $.get(url);
+			// xhr.done(function(result){
+			// 	console.log(result);
+			// 	let qtd = result.data.length;
 
-// 			xhr.done(function(result){
-// 				console.log(result);
-// 				let qtd = result.data.length;
+			// 	if (qtd > 0) {
 
-// 				if (qtd > 0) {
+			// 		let  numero = Math.floor(Math.random() * qtd);
+			// 		let image_url = result.data[numero].images.downsized.url;
+			// 		 document.getElementById("img_result").src = image_url;
 
-// 					let  numero = Math.floor(Math.random() * qtd);
-// 					let image_url = result.data[numero].images.downsized.url;
-// 					 document.getElementById("img_result").src = image_url;
+			// 	}
 
-// 				}
+			// });
 
-// 			});
+		// }
 
-// 		}
+		document.getElementById('result').innerHTML = busca;
 
-// 		document.getElementById('result').innerHTML = busca;
+	}
 
-// 	}
+	recognition.start();
 
-// 	recognition.start();
-
-
-// }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRecordRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -17,12 +20,51 @@ class HomeController extends AbstractController
     }
 
     /**
+     * @Route("/create-record", name="app_create_record")
+     */
+    public function create(Request $request): Response
+    {
+        return $this->json([
+            $request,
+            'message' => 'save success',
+        ], 200);
+    }
+
+    /**
      * @Route("/home", name="app_home")
      */
-    public function home(): Response
+    public function read(): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        return $this->render('home/index.html.twig');
+        
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        return $this->render('home/index.html.twig', [
+            'records' => $user->getUserRecords()->getValues(),
+        ]);
+    }
+
+    /**
+     * @Route("/update-record", name="app_update_record")
+     */
+    public function update(Request $request): Response
+    {
+        return $this->json([
+            $request,
+            'message' => 'update success',
+        ], 200);
+    }
+
+    /**
+     * @Route("/delete-record", name="app_delete_record")
+     */
+    public function delete(Request $request): Response
+    {
+        return $this->json([
+            $request,
+            'message' => 'delete success',
+        ], 200);
     }
 
 }
