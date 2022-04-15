10c3e27a6aa70a000a574cfc07055a2f69ca7917
razielrodrigues <raziel@upoutsourcing.com>
2022-04-15 14:22:17 -0300

    implementing easyadmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        // redirect to some CRUD controller
+        // $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        // return $this->redirect($routeBuilder->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // // you can also redirect to different pages depending on the current user
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // you can also render some template to display a proper Dashboard
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        return $this->render('bundles/EasyAdminBundle/index.html.twig');
+
+        // $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        // // ...set chart data and options somehow
+
+        // return $this->render('admin/my-dashboard.html.twig', [
+        //     'chart' => $chart,
+        // ]);
+
+        // return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            // the name visible to end users
+            ->setTitle('You speech we write')
+            // you can include HTML contents too (e.g. to link to an image)
+            // ->setTitle('<img src="..."> ACME <span class="text-small">Corp.</span>')
+
+            // the path defined in this method is passed to the Twig asset() function
+            // ->setFaviconPath('favicon.svg')
+
+            // the domain used by default is 'messages'
+            // ->setTranslationDomain('my-custom-domain')
+
+            // there's no need to define the "text direction" explicitly because
+            // its default value is inferred dynamically from the user locale
+            // ->setTextDirection('ltr')
+
+            // set this option if you prefer the page content to span the entire
+            // browser width, instead of the default design which sets a max width
+            // ->renderContentMaximized()
+
+            // set this option if you prefer the sidebar (which contains the main menu)
+            // to be displayed as a narrow column instead of the default expanded design
+            // ->renderSidebarMinimized()
+
+            // by default, users can select between a "light" and "dark" mode for the
+            // backend interface. Call this method if you prefer to disable the "dark"
+            // mode for any reason (e.g. if your interface customizations are not ready for it)
+            // ->disableDarkMode()
+
+            // by default, all backend URLs are generated as absolute URLs. If you
+            // need to generate relative URLs instead, call this method
+            // ->generateRelativeUrls()
+        ;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::section('Users');
+        // yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        // yield MenuItem::linkToUrl('Search in Google', 'fab fa-google', 'https://google.com');
+        // yield MenuItem::linkToLogout('Logout', 'fa fa-exit');
+        // yield MenuItem::linkToExitImpersonation('Stop impersonation', 'fa fa-exit');
+    }
+
+    // public function configureUserMenu(UserInterface $user): UserMenu
+    // {
+        // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        // return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            // ->setName($user->getFullName())
+            // use this method if you don't want to display the name of the user
+            // ->displayUserName(false)
+
+            // you can return an URL with the avatar image
+            // ->setAvatarUrl('https://...')
+            // ->setAvatarUrl($user->getProfileImageUrl())
+            // use this method if you don't want to display the user image
+            // ->displayUserAvatar(false)
+            // you can also pass an email address to use gravatar's service
+            // ->setGravatarEmail($user->getMainEmailAddress())
+
+            // you can use any type of menu item, except submenus
+            // ->addMenuItems([
+            //     MenuItem::linkToRoute('My Profile', 'fa fa-id-card', '...', ['...' => '...']),
+            //     MenuItem::linkToRoute('Settings', 'fa fa-user-cog', '...', ['...' => '...']),
+            //     MenuItem::section(),
+            //     MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
+            // ]);
+    // }
+
+}
