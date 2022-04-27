4bef69099d498fecc8ca21d44585595e655031b7
razielrodrigues <raziel@upoutsourcing.com>
2022-04-27 01:13:19 -0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -am "deploy"
+git push heroku master
+git push heroku main
+
+# mysql://bfff28e7229e4b:bef6c00e@us-cdbr-east-05.cleardb.net/heroku_201a8405690d62f?reconnect=true
